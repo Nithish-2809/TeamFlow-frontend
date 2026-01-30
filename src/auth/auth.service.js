614cb760f68739812c173b7bc592da6f5ec00032
Nithish Reddy <nithishreddy2809@gmail.com>
2026-01-30 22:44:43 +0530

    added google auth in auth.service.js

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -4,10 +4,14 @@ import { loginUser } from "./auth.api"
 
 const loginAndSetAuth = async (loginDetails)=> {
     const data = await loginUser(loginDetails)
-    
     useAuthStore.getState().setAuth(data.user,data.token)
-
     return data.user
+}
+
+const googleLoginAndSetAuth = async (idToken) => {
+  const { user, token } = await googleLoginUser(idToken)
+  useAuthStore.getState().setAuth(user, token)
+  return user
 }
 export {loginAndSetAuth}
 
