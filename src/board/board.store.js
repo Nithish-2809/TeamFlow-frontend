cb280ed9e460b2e86f18308157c1839da93976de
Nithish Reddy <nithishreddy2809@gmail.com>
2026-02-02 20:48:39 +0530

    added board.store.js

---
+++ src/board/board.store.js
@@ -0,0 +1,60 @@
+import { create } from "zustand"
+import boardApi from "./board.api"
+import { useAuthStore } from "../auth/auth.store"
+
+const useBoardStore = create((set) => ({
+  boards: [],
+  pendingBoards: [],
+  loading: false,
+  error: null,
+
+  
+  fetchBoards: async () => {
+    try {
+      set({ loading: true, error: null })
+
+      const token = useAuthStore.getState().token
+
+      const approvedRes = await boardApi.getMyBoards(token)
+      const pendingRes = await boardApi.getPendingBoards(token)
+
+      set({
+        boards: approvedRes.boards || [],
+        pendingBoards: pendingRes.boards || [],
+        loading: false
+      })
+    } catch (err) {
+      set({
+        error: err.response?.msg || "Failed to fetch boards",
+        loading: false
+      })
+    }
+  },
+
+
+  createBoard: async (boardDetails) => {
+    try {
+      const token = useAuthStore.getState().token
+
+      const res = await boardApi.createBoard(boardDetails, token)
+
+      set((state) => ({
+        boards: [res.board, ...state.boards] 
+      }))
+
+      return res
+    } catch (err) {
+      throw err
+    }
+  },
+
+  clearBoards: () => {
+    set({
+      boards: [],
+      pendingBoards: [],
+      error: null
+    })
+  }
+}))
+
+export { useBoardStore }
