cdb2f4b0e86dce706e69f7b30595fbaec0b35c78
Nithish Reddy <nithishreddy2809@gmail.com>
2026-02-14 23:15:46 +0530

    added list.api.js

---
+++ src/api/list.api.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+const listApi = axios.create({
+  baseURL: "http://localhost:2231/api",
+  withCredentials: true
+})
+
+
+const getBoardLists = async (token, boardId) => {
+  const res = await listApi.get(`/boards/${boardId}/lists`, {
+    headers: { Authorization: `Bearer ${token}` }
+  })
+  return res.data
+}
+
+
+const createList = async (token, boardId, name) => {
+  const res = await listApi.post(
+    `/boards/${boardId}/lists`,
+    { name },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+  return res.data
+}
+
+
+const renameList = async (token, boardId, listId, name) => {
+  const res = await listApi.patch(
+    `/boards/${boardId}/lists/${listId}`,
+    { name },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+  return res.data
+}
+
+
+const deleteList = async (token, boardId, listId) => {
+  const res = await listApi.delete(
+    `/boards/${boardId}/lists/${listId}`,
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+  return res.data
+}
+
+
+const reorderLists = async (token, boardId, orderedListIds) => {
+  const res = await listApi.patch(
+    `/boards/${boardId}/lists/reorder`,
+    { orderedListIds },
+    { headers: { Authorization: `Bearer ${token}` } }
+  )
+  return res.data
+}
+
+export { getBoardLists, createList, renameList, deleteList, reorderLists }
