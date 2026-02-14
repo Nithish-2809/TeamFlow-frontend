4dd52d6eb47249dffb07b40e42b79b73013f8389
Nithish Reddy <nithishreddy2809@gmail.com>
2026-02-14 23:34:54 +0530

    added BoardList

---
+++ src/components/board/BoardList.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react"
+import { useSortable } from "@dnd-kit/sortable"
+import { CSS } from "@dnd-kit/utilities"
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable"
+import { useBoardPageStore } from "../../store/boardPage.store"
+import TaskCard from "./TaskCard"
+import CreateTaskButton from "./CreateTaskButton"
+import ListMenu from "./ListMenu"
+import "../../styles/BoardList.css"
+
+function BoardList({ list, tasks, boardId, isAdmin }) {
+  const [isCreatingTask, setIsCreatingTask] = useState(false)
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: list._id,
+    data: {
+      type: 'list',
+      list,
+    },
+  })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : 1,
+  }
+
+  return (
+    <div ref={setNodeRef} style={style} className="board-list">
+      <div className="list-header" {...attributes} {...listeners}>
+        <h3 className="list-title">{list.name}</h3>
+        <div className="list-actions">
+          <span className="task-count">{tasks.length}</span>
+          {isAdmin && <ListMenu list={list} boardId={boardId} />}
+        </div>
+      </div>
+
+      <div className="list-tasks">
+        <SortableContext
+          items={tasks.map(task => task._id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {tasks.map((task) => (
+            <TaskCard
+              key={task._id}
+              task={task}
+              boardId={boardId}
+              listId={list._id}
+            />
+          ))}
+        </SortableContext>
+      </div>
+
+      <CreateTaskButton
+        boardId={boardId}
+        listId={list._id}
+        isCreating={isCreatingTask}
+        setIsCreating={setIsCreatingTask}
+      />
+    </div>
+  )
+}
+
+export default BoardList
