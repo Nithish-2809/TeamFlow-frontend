80360b54f3c66b70b8b4ffe3e9ffd8dfeeaa4c60
Nithish Reddy <nithishreddy2809@gmail.com>
2026-02-21 21:43:19 +0530

    modifies incite.api

--- src/api/invite.api.js
+++ src/api/invite.api.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
 const inviteApi = axios.create({
-  baseURL: "http://localhost:2231/api/invite",
+  baseURL: "http://localhost:2231/api/boards",
   withCredentials: true
-})
+});
 
+// Validate invite token (public)
+const validateInviteToken = async (token) => {
+  const res = await inviteApi.get(`/invite/${token}`);
+  return res.data;
+};
+
+// Join via invite link (requires auth)
 const joinViaInviteLink = async (token, authToken) => {
-  const res = await inviteApi.post(`/join/${token}`, {}, {
+  const res = await inviteApi.post(`/invite/${token}/join`, {}, {
     headers: {
       Authorization: `Bearer ${authToken}`
     }
-  })
-  return res.data
-}
+  });
+  return res.data;
+};
 
-export { joinViaInviteLink }
+export { validateInviteToken, joinViaInviteLink };
