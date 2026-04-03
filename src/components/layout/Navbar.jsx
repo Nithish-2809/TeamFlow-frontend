2b7fb83dafe45df11a977606a1fb4d11da4bac00
Nithish Reddy <nithishreddy2809@gmail.com>
2026-04-03 15:13:03 +0530

    deleted file

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
 import { useAuthStore } from "../../store/auth.store"
-import ConfirmationModal from "../modals/Confirmationmodal"
+import ConfirmationModal from "../modals/ConfirmationModal"
 import "../../styles/Navbar.css"
 
 function Navbar() {
